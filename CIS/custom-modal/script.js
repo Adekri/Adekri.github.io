f230ece411fd407bac025beabff945c1e0320811
Adekri <247567@vutbr.cz>
2026-03-14 14:45:08 +0100

    presuny

---
+++ CIS/custom-modal/script.js
@@ -0,0 +1,8 @@
+    function showInfoModal(text){
+        const textEl = document.getElementById("infoModalText")
+        textEl.textContent = text
+
+        const modalEl = document.getElementById("infoModal")
+        const modal = new bootstrap.Modal(modalEl)
+        modal.show()
+    }
